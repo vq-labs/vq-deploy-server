7294b09a4f49dc18e1597b3261e81e841f1c83e8
Sercan AKMAN <sercanakman@gmail.com>
2018-03-07 19:23:28 +0300

    test

--- index.js
+++ index.js
@@ -5,16 +5,32 @@ const createHandler = require('github-webhook-handler')
 const spawn = require('child_process').spawn
 const handler = createHandler({ path: process.env.HOOK_PATH, secret: process.env.HOOK_SECRET })
 
+const runCommand = (cmd, args = [], cb, endCb) => {
+    const process = spawn(cmd, args);
+
+    process.stdout.on('data', data => {
+        cb(data);
+    });
+
+    process.stderr.on('data', data => {
+        cb(undefined, data);
+    });
+
+    process.on('close', code => {
+        endCb(code !== 0 ? false : true);
+    });
+}
+
 http.createServer((req, res) => {
     console.log(`[VQ-DEPLOY-SERVER] has started running on port ${process.env.SERVER_PORT}`);
-  handler(req, res, (err) => {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(7777)
+    handler(req, res, (err) => {
+        res.statusCode = 404
+        res.end('no such location')
+    });
+}).listen(7777);
 
 handler.on('error', (err) => {
-  console.error('Error:', err.message)
+  console.error('Error:', err.message);
 })
 
 handler.on('push', (event) => {
@@ -22,20 +38,21 @@ handler.on('push', (event) => {
     const branchName = event.payload.ref.replace("refs/heads/", "");
     if (branchName === process.env.HOOK_BRANCH) {
         console.log(`[VQ-DEPLOY-SERVER] received a push event for %s repository %s branch`, repoName, branchName);
+        runCommand(
+            'ls',
+            ['-lh'],
+            (data, err) => {
+                if (err) {
+                    console.log('ERR', err);
+                }
+
+                console.log('DATA', data);
+            },
+            success => {
+                if (success) {
+                    console.log('process success');
+                }
+            }
+        );
     }
-
-})
-
-const npm = spawn("s3-deploy", args, { cwd: './build' });
-
-npm.stdout.on('data', data => {
-    console.log(`stdout: ${data}`);
-});
-
-npm.stderr.on('data', data => {
-    console.log(`stderr: ${data}`);
-});
-
-npm.on('close', code => {
-    cb(code !== 0 ? 'error in build' : null);
 });
