6a0f55b539df54ac4375c2796925a4772104d401
Sercan AKMAN <sercanakman@gmail.com>
2018-03-07 19:03:01 +0300

    added spawn code for deployment

--- index.js
+++ index.js
@@ -2,29 +2,44 @@ require('dotenv').config();
 
 var http = require('http')
 var createHandler = require('github-webhook-handler')
+var spawn = require('child_process').spawn
 var handler = createHandler({ path: process.env.HOOK_PATH, secret: process.env.HOOK_SECRET })
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
+http.createServer((req, res) => {
+  handler(req, res, (err) => {
     res.statusCode = 404
     res.end('no such location')
   })
 }).listen(7777)
 
-handler.on('error', function (err) {
+handler.on('error', (err) => {
   console.error('Error:', err.message)
 })
 
-handler.on('push', function (event) {
+handler.on('push', (event) => {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
 })
 
-handler.on('issues', function (event) {
+handler.on('issues', (event) => {
   console.log('Received an issue event for %s action=%s: #%d %s',
     event.payload.repository.name,
     event.payload.action,
     event.payload.issue.number,
     event.payload.issue.title)
 })
+
+var npm = spawn("s3-deploy", args, { cwd: './build' });
+
+npm.stdout.on('data', data => {
+    console.log(`stdout: ${data}`);
+});
+
+npm.stderr.on('data', data => {
+    console.log(`stderr: ${data}`);
+});
+
+npm.on('close', code => {
+    cb(code !== 0 ? 'error in build' : null);
+});
