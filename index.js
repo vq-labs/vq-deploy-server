b5f003e0671373ece10d500cdd509c32e83deab6
Sercan AKMAN <sercanakman@gmail.com>
2018-03-07 18:41:24 +0300

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
+var handler = createHandler({ path: process.env.PATH, secret: process.env.SECRET })
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
