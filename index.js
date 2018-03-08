f5bba8e97b5d6250d35e059091588dd6dd1eac4f
Sercan AKMAN <sercanakman@gmail.com>
2018-03-08 18:06:37 +0300

    test

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function bytesToSize(bytes) {
     if (bytes === 0) return 'n/a'
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
     if (i === 0) return `${bytes} ${sizes[i]})`
-    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
+    return `${(bytes / (1024 * i)).toFixed(1)} ${sizes[i]}`
   }
 
 const convertMillisToTime = (millis) => {
