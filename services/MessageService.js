6b61f33878e7baf044b2fa0b42f0eb152728d9bb
Sercan AKMAN <sercanakman@gmail.com>
2018-03-13 02:03:55 +0300

    test

--- services/MessageService.js
+++ services/MessageService.js
@@ -40,6 +40,7 @@ class MessageService {
         if (typeof path === 'string') {
             // if only a string is passed as path
             // it gets the corresponding message template from MESSAGES
+            console.log('PATH', path);
             let message = _get(MESSAGES, path);
     
             attachments.push(
