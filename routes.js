63e968703a39522073acb4f96296e450e88113ab
Sercan AKMAN <sercanakman@gmail.com>
2018-03-13 05:15:16 +0300

    test

--- routes.js
+++ routes.js
@@ -2,7 +2,11 @@ const PM2Service = require('./services/PM2Service');
 
 module.exports = function(error, req, res) {
     if (req.method === 'POST' && req.url === '/status') {
-        return PM2Service(res);
+        if (process.env.SLACK_SERVER_STATUS) {
+            return PM2Service(res);
+        }
+        
+        return false;
     } else {
         res.statusCode = 404;
         return res.end('No such path exists');
